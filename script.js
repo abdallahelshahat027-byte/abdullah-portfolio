e2b004575510ca78660623f3263de38cbe9178c8
abdallahelshahat027-byte <abdallahelshahat027@gmail.com>
2026-07-04 20:49:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-const text = "بورتفوليو عبدالله الشحات";
+/* Cursor Glow */
 
-let i = 0;
+const cursor=document.createElement("div");
 
-function typeWriter(){
+cursor.style.width="18px";
+cursor.style.height="18px";
+cursor.style.border="2px solid #00c6ff";
+cursor.style.borderRadius="50%";
+cursor.style.position="fixed";
+cursor.style.pointerEvents="none";
+cursor.style.transition=".08s";
+cursor.style.zIndex="999999";
 
-if(i < text.length){
+document.body.appendChild(cursor);
 
-document.getElementById("typing").innerHTML += text.charAt(i);
+document.addEventListener("mousemove",e=>{
 
-i++;
+cursor.style.left=e.clientX-9+"px";
+cursor.style.top=e.clientY-9+"px";
 
-setTimeout(typeWriter,80);
+});
 
-}
+/* Parallax */
 
-}
+document.addEventListener("mousemove",e=>{
 
-typeWriter();
+const hero=document.querySelector(".hero");
+
+let x=(e.clientX/window.innerWidth)*20;
+
+let y=(e.clientY/window.innerHeight)*20;
+
+hero.style.backgroundPosition=`${50+x/3}% ${50+y/3}%`;
+
+});
